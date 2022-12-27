a9477bf50e84f8f4aeee875f93dd1ba03e1ec03e
JasonWu00 <zehong.wu17@myhunter.cuny.edu>
2022-12-27 16:38:53 -0500

    a c++ file to make deriving stats easier and less manually intensive

---
+++ stat-calculator.cpp
@@ -0,0 +1,9 @@
+//C++ file produced by the mod author to make stat calculation easier.
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int main() {
+    std::cout << "Event Horizon ES Mod stat calculator is now active." << std::endl;
+}
